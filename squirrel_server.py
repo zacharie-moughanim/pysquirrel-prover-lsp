f066728fd77322c8a519ef42c4031241f53ae0c1
ZachMGH <zachariemoughanim@gmail.com>
2026-07-20 22:35:11 +0200

    Add comments

--- squirrel_server.py
+++ squirrel_server.py
@@ -38,12 +38,10 @@ def recv_until(n : int) -> str :
 
 def LSPRecv() -> dict[Any, Any] :
   """ Receives a LSP request, returns a dictionary containg the JSON received. """
-  # send("Entering LSPRecv...", end="")
   # Reading header
   reading_header : bool = True
   content_length : int = -1
   while reading_header :
-    # send("Looping while reading header...", end="")
     hline : str = recv()
     if hline == '' :
       reading_header = False
@@ -60,8 +58,12 @@ def LSPRecv() -> dict[Any, Any] :
   assert(isinstance(parsed_payload, dict))
   return parsed_payload
 
+# The string output by squirrel in interactive mode that we interpret as "Squirrel is waiting for input"
 squirrelInputIndicator : str = "[>  "
 
+# TODO multiple sessions
+
+# First waiting for a message indicating the beginning of a proof session, containing the path to squirrel.
 data = LSPRecv()
 if DEBUG_MODE :
   send(json.dumps({"method":"vsquirrel/debug", "data":str(data)}), end="")
@@ -70,12 +72,15 @@ if "pathToSquirrel" in data :
     send(json.dumps({"method":"vsquirrel/debug", "data":f"path to squirrel received!{data["pathToSquirrel"]}"}), end="")
   squirrelPath = data["pathToSquirrel"]
 
+# Keeping track of last id of a request to answer following the LSP
 last_id_request : int = data["id"]
 
 # TODO error management, display message "maybe wrong path to squirrel"
+# Spawning a squirrel instance
 squirrelInstance = subprocess.Popen([squirrelPath, "-i"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 while True :
+  # Loading squirrel's output until [squirrelInputIndicator] is output by squirrel.
   buf : bytes = b""
   squirrelIsWaitingForInput : bool = False
   while not(squirrelIsWaitingForInput) :
@@ -86,7 +91,9 @@ while True :
         squirrelIsWaitingForInput = (lastChunk == squirrelInputIndicator)
       except UnicodeDecodeError :
         squirrelIsWaitingForInput = False
+  # Sending to LSP client the output of squirrel.
   LSPAnswerQuery(last_id_request, buf.decode())
+  # Waiting for LSP client's request (e.g. a proof command to process)
   data = LSPRecv()
   if "proofCommand" not in data :
     send(json.dumps({"method":"vsquirrel/error", "data":"No proof command in request"}), end="")
@@ -94,6 +101,7 @@ while True :
     proofCommand = data["proofCommand"]
     assert(isinstance(proofCommand, str))
     if proofCommand != "" :
+      # Executing proofCommand in squirrel
       squirrelInstance.stdin.write((proofCommand + "\n").encode())
       squirrelInstance.stdin.flush()
   last_id_request = data["id"]
